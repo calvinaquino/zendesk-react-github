0557b5104b48aa4768804728036113a34eb33c64
Calvin Aquino <calvin.de.aquino@clio.com>
2019-07-28 15:29:30 -0700

    basic flow without auth

---
+++ .react-playground/index.js
@@ -0,0 +1,8 @@
+
+        import React from 'react';
+        import ReactDom from 'react-dom';
+        import Playground from 'react-playground-kit';
+        import * as module from '/Users/calvinaqunio/Projects/zendesk-react-github/src/app.js';
+
+        ReactDom.render(React.createElement(Playground, { module }), document.getElementById('app'));
+    
--- src/app.js
+++ src/app.js
@@ -5,43 +5,111 @@ import { connect } from 'react-redux';
 
 function App(props) {
   const [list, setList] = useState([]);
+  const [token, setToken] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const { users } = props;
   const { requester = {} } = users;
+
+  let passwordRef = undefined;
 
   const onKeyDown = (event) => {
     if (event.key === 'Enter') {
       const { value } = event.target;
 
-      fetch(`https://api.github.com/search/issues?q=${value}+user:calvinaquino`,{
+      // fetch(`https://api.github.com/search/issues?q=${value}+org:clio`,{
+      fetch(`https://api.github.com/search/issues?q=${value}+user:calvinaquino+repo:test-private`,{
+      // fetch(`https://api.github.com/authorizations`,{
         headers: {
           'Accept': 'application/vnd.github.v3+json',
+          // 'Authorization': 'Bearer fd5c0e1be3c0eccc6b0cc613018242bfc7b8c088',
+          // 'Authenticate': `Basic ${Buffer.from().toString()}`,
         },
       },)
       .then(res => res.json())
-      .then(data => setList(data.items))
+      .then(data => {
+        console.log(data);
+        setList(data.items)
+      })
+      .catch(error => console.log(error));
     }
   }
 
-  return (
-    <div className="App">
-      <header className="App-header">
+  const onLoginKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      setUsername(event.target.value)
+      passwordRef.focus()
+    }
+  }
+
+  const onPasswordKeyDown = () => {
+    if (event.key === 'Enter') {
+      const { value } = event.target;
+      fetch(`https://api.github.com/auth`,{
+        method: 'POST',
+        headers: {
+          'Accept': 'application/vnd.github.v3+json',
+          'Authorization': 'Basic ' + new Buffer(username + ':' + value).toString('base64'),
+          'scopes': 'repo',
+        },
+      },)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => console.log(error));
+    }
+  }
+
+  const renderLogin = () => {
+    return (
+      <div className="App">
+        <header className="App-header">
         <input
-          type="text"
-          className="input"
-          id="addInput"
-          placeholder="Search Github issues"
-          onKeyDown={onKeyDown}
-        />
-      </header>
-      {list.map(item => (
-        <div key={item.html_url} className="item">
-          <a href={item.html_url} className="item-title">{item.title}</a>
-          <p className="item-body">{item.body}</p>
-        </div>
-      ))}
-    </div>
-  );
+            type="text"
+            className="input"
+            id="login"
+            placeholder="Login"
+            onKeyDown={onLoginKeyDown}
+          />
+          <input
+            ref={input => passwordRef = input}
+            type="password"
+            className="input"
+            id="password"
+            placeholder="Password"
+            onKeyDown={onPasswordKeyDown}
+          />
+        </header>
+      </div>
+    );
+  }
+
+  const renderSearch = () => {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <input
+            type="text"
+            className="input"
+            id="addInput"
+            placeholder="Search Github issues"
+            onKeyDown={onKeyDown}
+          />
+        </header>
+        {list.map(item => (
+          <div key={item.html_url} className="item">
+            <a href={item.html_url} className="item-title" target="_blank">{item.title}</a>
+            <p className="item-body">{item.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  // return (token ? renderSearch() : renderLogin());
+  return renderSearch();
 }
 
 App.propTypes = {
